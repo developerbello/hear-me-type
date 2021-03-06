183c338fef4b654f5cca2bf40c786176c422e731
developerbello <developerbello@gmail.com>
2021-03-06 10:42:52 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# hear-me-type
+
+**Clone and run for a quick way to see Electron in action.**
+
+```bash
+# Clone this repository
+git clone https://github.com/hear-me-type
+# Go into the repository
+cd hear-me-type
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+
+## License
+
+[CC0 1.0 (Public Domain)](LICENSE.md)
---
+++ functions.js
@@ -0,0 +1,116 @@
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    
+    case 48:
+      document.getElementById('A').play();
+      break
+    case 49:
+      document.getElementById('B').play();
+      break;
+    case 50:
+      document.getElementById('C').play();
+      break;
+    case 51:
+      document.getElementById('D').play();
+      break;
+    case 48:
+      document.getElementById('E').play();
+      break;
+    case 49:
+      document.getElementById('F').play();
+      break;      
+    case 50:
+      document.getElementById('G').play();
+      break;
+    case 51:
+      document.getElementById('H').play();
+      break;  
+    case 52:
+      document.getElementById('I').play();
+      break;  
+    case 53:
+      document.getElementById('J').play();
+      break;  
+    case 54:
+      document.getElementById('K').play();
+      break;
+    case 55:
+      document.getElementById('L').play();
+      break;
+    case 56:
+      document.getElementById('M').play();
+      break;
+    case 57:
+      document.getElementById('N').play();
+      break;
+    case 58:
+      document.getElementById('O').play();
+      break;
+    case 60:
+      document.getElementById('P').play();
+      break;
+    case 61:
+      document.getElementById('Q').play();
+      break
+    case 62:
+      document.getElementById('R').play();
+      break;
+    case 63:
+      document.getElementById('S').play();
+      break;
+    case 64:
+      document.getElementById('T').play();
+      break;
+    case 65:
+      document.getElementById('U').play();
+      break;
+    case 66:
+      document.getElementById('V').play();
+      break;      
+    case 67:
+      document.getElementById('W').play();
+      break;
+    case 68:
+      document.getElementById('X').play();
+      break;  
+    case 69:
+      document.getElementById('Y').play();
+      break;
+    case 71:
+      document.getElementById('0').play();
+      break;  
+    case 72:
+      document.getElementById('Z').play();
+      break;  
+    case 73:
+      document.getElementById('1').play();
+      break;
+    case 74:
+      document.getElementById('2').play();
+      break;
+    case 75:
+      document.getElementById('3').play();
+      break;
+    case 76:
+      document.getElementById('4').play();
+      break;
+    case 77:
+      document.getElementById('5').play();
+      break;
+    case 78:
+      document.getElementById('6').play();
+      break;
+    case 79:
+      document.getElementById('7').play();
+        break;
+    case 80:
+      document.getElementById('8').play();
+      break;
+    case 81:
+      document.getElementById('9').play();
+      break;
+    
+      default:
+        console.log('Key is not found!');
+  }
+}
